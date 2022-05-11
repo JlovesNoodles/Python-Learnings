e72774d10631b2526ce39363a4188370a06ec735
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2022-05-11 13:08:52 +0800

    Create Primenumber.py

---
+++ Primenumber.py
@@ -0,0 +1,16 @@
+number = input ('Enter the number: ')
+
+
+for i in number:
+    if int(number) % 2 == 0:
+        
+        print("The number is Not Prime")
+        for num in range (0,int(number),2):
+
+            print(num + 1)
+        break
+    else:
+        print("The number is Prime")
+        break
+        
+
