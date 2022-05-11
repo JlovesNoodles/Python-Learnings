3093d53dd5296962adb7e151e5bcf892df16eeb9
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2022-05-11 13:15:01 +0800

    Update Primenumber.py

--- Primenumber.py
+++ Primenumber.py
@@ -6,11 +6,12 @@ for i in number:
         
         print("The number is Not Prime")
         for num in range (0,int(number),2):
-
-            print(num + 1)
+            print(num + 2)
         break
     else:
         print("The number is Prime")
+        for num in range (0,int(number),2):
+            print(num + 1)
         break
         
 
