48de607b9f3842f7265980e1334022dfcc81c4f4
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2022-05-12 08:20:02 +0800

    Create WhileLoopMultiply.py

---
+++ WhileLoopMultiply.py
@@ -0,0 +1,10 @@
+
+x = int(input ("enter the number to print the multiplication table: "))
+#x = 1
+num1 = 1
+
+while num1 <= x :
+    print('{} * {} = {}'.format(num1,x, num1*x))
+    #num1 = num1 + 1
+    num1 = num1 + 1
+   
