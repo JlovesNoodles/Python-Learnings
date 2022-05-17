490fcabebf87730f3807ca50c9644f2cee2fc0cb
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2022-05-17 14:07:46 +0800

    Create Three_Cup_Monte.py

---
+++ Three_Cup_Monte.py
@@ -0,0 +1,29 @@
+from random import shuffle
+
+from random import shuffle
+
+def shuffle_cup():
+    cups = [' ' , '0', ' ']
+    shuffle(cups)
+    return cups
+
+V_shuffle_cup = shuffle_cup()
+
+def user_guess():
+    guess = input("Choose A number between 0, 1, 2 -> ")
+    return guess
+
+guess = user_guess()
+
+
+def the_game(V_shuffle_cup,guess):
+    if V_shuffle_cup[int(guess)] == '0':
+        print ("You got it right!")
+        print (V_shuffle_cup)
+    else:
+        print("Try again")
+        print(V_shuffle_cup)
+        
+        
+        
+the_game(V_shuffle_cup,guess)
