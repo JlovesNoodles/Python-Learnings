366b4ad5d5f1470dc08240d17c98e524c62d6301
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2022-05-17 09:31:12 +0800

    Update Primenumber.py

--- Primenumber.py
+++ Primenumber.py
@@ -15,3 +15,17 @@ for i in number:
         break
         
 
+
+        ######################################################################################
+def prime_checker(number):
+    for i in range (number):
+        if number % 2 == 0:
+            print ("The number is Not Prime")
+            for num in range (0,int(number),2):
+                print (num + 2)
+            break
+        else:
+            print ("The number is Prime")
+            for num in range (0,int(number),2):
+                print(num + 1)
+            break
